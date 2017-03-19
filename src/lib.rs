23e2a3bb5b95d9aa0ba342acecde53ac98b9e8bb
Luca Barbato <lu_zero@gentoo.org>
2017-03-19 19:56:12 +0000

    bindings: More encoder

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,24 @@ extern crate x264_sys as ffi;
 use std::mem;
 use ffi::x264::*;
 use std::ptr::null;
+use std::os::raw::c_int;
 
 // TODO: Provide a builder API instead?
-pub struct X264Param {
+pub struct Param {
     par: x264_param_t,
 }
 
-impl X264Param {
-    pub fn new() -> X264Param {
+impl Param {
+    pub fn new() -> Param {
         let mut par = unsafe { mem::uninitialized() };
 
         unsafe {
             x264_param_default(&mut par as *mut x264_param_t);
         }
 
-        X264Param { par: par }
+        Param { par: par }
     }
-    pub fn default_preset(tune: Option<&str>,
-                          preset: Option<&str>)
-                          -> Result<X264Param, &'static str> {
+    pub fn default_preset(tune: Option<&str>, preset: Option<&str>) -> Result<Param, &'static str> {
         let mut par = unsafe { mem::uninitialized() };
 
         match unsafe {
@@ -32,18 +31,18 @@ impl X264Param {
                                             *const i8)
               } {
             -1 => Err("Invalid Argument"),
-            0 => Ok(X264Param { par: par }),
+            0 => Ok(Param { par: par }),
             _ => Err("Unexpected"),
         }
     }
-    pub fn apply_profile(&mut self, profile: &str) -> Result<X264Param, &'static str> {
+    pub fn apply_profile(mut self, profile: &str) -> Result<Param, &'static str> {
         match unsafe { x264_param_apply_profile(&mut self.par, profile.as_ptr() as *const i8) } {
             -1 => Err("Invalid Argument"),
             0 => Ok(self),
             _ => Err("Unexpected"),
         }
     }
-    pub fn param_parse(&mut self, name: &str, value: &str) -> Result<X264Param, &'static str> {
+    pub fn param_parse<'a>(mut self, name: &str, value: &str) -> Result<Param, &'static str> {
         match unsafe {
                   x264_param_parse(&mut self.par,
                                    name.as_ptr() as *const i8,
@@ -54,27 +53,94 @@ impl X264Param {
             _ => Err("Unexpected"),
         }
     }
+    pub fn set_dimension(mut self, height: usize, width: usize) -> Param {
+        self.par.i_height = height as c_int;
+        self.par.i_width = width as c_int;
+
+        self
+    }
 }
 
-pub struct X264 {
+// TODO: Expose a NAL abstraction
+pub struct NalData {
+    vec: Vec<u8>,
+}
+
+impl NalData {
+    fn with_capacity(capacity: usize) -> NalData {
+        NalData { vec: Vec::with_capacity(capacity) }
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.vec.as_slice()
+    }
+}
+
+pub struct Encoder {
     enc: *mut x264_t,
 }
 
-impl X264 {
-    pub fn open(mut par: X264Param) -> Result<X264, &'static str> {
+impl Encoder {
+    pub fn open(par: &mut Param) -> Result<Encoder, &'static str> {
         let enc = unsafe { x264_encoder_open(&mut par.par as *mut x264_param_t) };
 
         if enc.is_null() {
             Err("Out of Memory")
         } else {
-            Ok(X264 { enc: enc })
+            Ok(Encoder { enc: enc })
+        }
+    }
+
+    pub fn get_headers(&mut self) -> Result<NalData, &'static str> {
+        let mut nb_nal: c_int = 0;
+        let mut c_nals: *mut x264_nal_t = unsafe { mem::uninitialized() };
+
+        let bytes = unsafe {
+            x264_encoder_headers(self.enc,
+                                 &mut c_nals as *mut *mut x264_nal_t,
+                                 &mut nb_nal as *mut c_int)
+        };
+
+        if bytes < 0 {
+            Err("Encoding Headers Failed")
+        } else {
+            let mut data = NalData::with_capacity(bytes as usize);
+
+            for i in 0..nb_nal {
+                let nal = unsafe { Box::from_raw(c_nals.offset(i as isize)) };
+
+                let payload =
+                    unsafe { std::slice::from_raw_parts(nal.p_payload, nal.i_payload as usize) };
+
+                data.vec.extend_from_slice(payload);
+
+                mem::forget(payload);
+                mem::forget(nal);
+            }
+
+            Ok(data)
         }
     }
 }
 
-impl Drop for X264 {
+impl Drop for Encoder {
     fn drop(&mut self) {
         unsafe { x264_encoder_close(self.enc) };
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_open() {
+        let mut par = Param::new().set_dimension(640, 480);
+
+        let mut enc = Encoder::open(&mut par).unwrap();
+
+        let headers = enc.get_headers().unwrap();
+
+        println!("Headers len {}", headers.as_bytes().len());
     }
 }
